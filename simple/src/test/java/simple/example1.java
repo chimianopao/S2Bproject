02691e63e71a305f92ab32b7285598e822ccbbee
Theilor <theilor@gmail.com>
2017-09-29 17:02:31 -0300

    AGORA FUNCIONA
    Tem 3 q diferentes dentro de 3 formularios diferentes. Somente o
    formulario 3 é visivel. Fiz um xpath para ele.

--- simple/src/test/java/simple/example1.java
+++ simple/src/test/java/simple/example1.java
@@ -7,6 +7,8 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class example1 {
 
@@ -17,16 +19,30 @@ public class example1 {
 		
 		WebDriver driver = new ChromeDriver();
 		
-		String site = "http://www.girafa.com.br/";
+		String site = "http://www.girafa.com.br/Login";
+		//String site = "http://www.metric-conversions.org/pt-br/velocidade/nos-em-quilometros-por-hora.htm";
 		driver.navigate().to(site);
-		String title = driver.getTitle();
-		assertTrue(title.startsWith("girafa"));
+//		String title = driver.getTitle();
+//		assertTrue(title.startsWith("Eletrônicos, Eletrodomésticos, Smartphone | Girafa"));
 		
-		WebElement input = driver.findElement(By.name("buscaprodutoform"));
-		input.sendKeys("playstation");
+		//WebDriverWait wait = new WebDriverWait(driver, 5);
+		//wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("q")));
+		//body > div.container.no-padding > header > div.topoMobile.visible-xs.visible-sm
+		//html/body/div[1]/header/div[2]
+		//WebElement input = driver.findElement(By.xpath("//div[@class='topoMobile visible-xs visible-sm']//div[@id='q']"));
+		
+		
+		WebElement input = driver.findElement(By.xpath("//*[@id=\"buscaprodutoform3\"]//*[@id=\"q\"]"));
+		//WebElement input = driver.findElement(By.id("q"));
+		//WebElement input = driver.findElement(By.id("argumentConv"));
+		
+		//By.xpath("//div[@class='page']//div[@id='_loginButton']")
+		
+		
+		input.sendKeys("celular");
 		input.submit();
 		//Thread.sleep(500);
-		WebElement output = driver.findElement(By.id("aswer"));
+		WebElement output = driver.findElement(By.id("answer"));
 		String result = output.getText();
 		assertTrue(result.contains("2knots"));
 		//Thread.sleep(500);
