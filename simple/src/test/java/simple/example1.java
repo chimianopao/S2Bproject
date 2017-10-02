d75fdd714aba93ac5c425feac4f532332c626c50
Theilor <theilor@gmail.com>
2017-10-02 16:36:52 -0300

    só não joga no carrinho

--- simple/src/test/java/simple/example1.java
+++ simple/src/test/java/simple/example1.java
@@ -19,7 +19,7 @@ public class example1 {
 		
 		WebDriver driver = new ChromeDriver();
 		
-		String site = "http://www.girafa.com.br/Login";
+		String site = "http://www.girafa.com.br";
 		//String site = "http://www.metric-conversions.org/pt-br/velocidade/nos-em-quilometros-por-hora.htm";
 		driver.navigate().to(site);
 //		String title = driver.getTitle();
@@ -27,14 +27,25 @@ public class example1 {
 		
 		
 		WebElement input = driver.findElement(By.xpath("//*[@id=\"buscaprodutoform3\"]//*[@id=\"q\"]"));
-		//WebElement input = driver.findElement(By.id("q"));
+		
 		
 		input.sendKeys("celular");
 		input.submit();
+		
+		driver.findElement(By.xpath("//a[@href='/Telefonia/samsung/smartphone-samsung-galaxy-j5-prime-dourado-dual-chip-32gb-tela-5-4g-camera-13mp-quad-core-1-4-ghz.htm']")).click();
+		
+		driver.findElement(By.id("calculoFrete")).click();
+		input = driver.findElement(By.id("postalCodeID1"));
+		input.sendKeys("91130020");
+		input.submit();
+		
+		driver.findElement(By.xpath("//*[@class=\"produto\"]//*a[href=\"javascript:comprar()\"]")).click();
+		
+		//<a href="javascript:comprar()"><span class="add-to-cart"> <span class="action-wrapper"> </span> </span> </a>
 		//Thread.sleep(500);
-		WebElement output = driver.findElement(By.id("answer"));
-		String result = output.getText();
-		assertTrue(result.contains("2knots"));
+		//WebElement output = driver.findElement(By.id("answer"));
+	//	String result = output.getText();
+	//	assertTrue(result.contains("2knots"));
 		//Thread.sleep(500);
 		driver.quit();
 		
