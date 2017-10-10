84c6701099b7ad550e60d7169bcc8425c6267df1
Theilor <theilor@gmail.com>
2017-10-10 16:53:30 -0300

    preenche o cadastro, mas por algum motivo não copia o cpf

--- simple/src/test/java/simple/example1.java
+++ simple/src/test/java/simple/example1.java
@@ -15,7 +15,7 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 public class example1 {
 /*
 	@Test
-	public void test() throws InterruptedException {
+	public void testCompraComFrete() throws InterruptedException {
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\teste\\Downloads\\chromedriver_win32\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		
@@ -39,12 +39,14 @@ public class example1 {
 		
 	}
 	
-	//*[@id="user-popup-window"]/div/div[2]/div/ul/li[1]
+	
 	
 	
 	*/
+	
+	/*
 	@Test
-	public void test2() {
+	public void testChat() throws InterruptedException {
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\teste\\Downloads\\chromedriver_win32\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		
@@ -53,9 +55,86 @@ public class example1 {
 		driver.navigate().to("https://www.girafa.com.br/chat-zoo/index.php/por/?site=girafa");
 		WebElement input = driver.findElement(By.name("Username"));
 		input.submit();
+		
 		WebElement element = driver.findElement(By.xpath("//*[@id=\"user-popup-window\"]/div/div[2]/div/ul/li[1]"));
-		assertTrue(element.getText().contains("Por favor, digite seu nome"));
+		assertTrue(element.getText().contains("Por favor"));
 		
 	}
-
+*/
+	
+	@Test
+	public void testCadastro() throws InterruptedException {
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\teste\\Downloads\\chromedriver_win32\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		
+		
+		driver.navigate().to("http://www.geradordecpf.org/");
+		WebElement element = driver.findElement(By.id("btn-gerar-cpf"));
+		element.click();
+		Thread.sleep(5000);
+		WebElement output = driver.findElement(By.id("numero"));
+		String cpf = new String();
+		cpf = output.getText();
+	
+		
+		String site = "http://www.girafa.com.br";
+		driver.navigate().to(site);
+		element = driver.findElement(By.xpath("/html/body/div[1]/header/div[4]/div[1]/div[2]/div[4]/a")); //clica em login
+		element.click();
+		
+		element = driver.findElement(By.xpath("/html/body/div[1]/div[3]/aside[2]/div/div/div[3]/div[2]/div[2]/a")); //clica em cadastro
+		element.click();
+		
+		WebElement input = driver.findElement(By.id("email"));
+		input.sendKeys("aaaaaaaaaaaa@aaaaaaaaaa.com");
+		input = driver.findElement(By.id("confirmarEmail"));
+		input.sendKeys("aaaaaaaaaaaa@aaaaaaaaaa.com");
+		input = driver.findElement(By.id("senha"));
+		input.sendKeys("12345");
+		input = driver.findElement(By.id("confirmarSenha"));
+		input.sendKeys("12345");
+		input = driver.findElement(By.id("cep"));
+		input.sendKeys("91010005");
+		input = driver.findElement(By.id("numero"));
+		input.sendKeys("1");
+		input = driver.findElement(By.id("complemento"));
+		input.sendKeys("casa");
+		
+		input = driver.findElement(By.id("nome"));
+		input.sendKeys("Joao");
+		input = driver.findElement(By.id("data"));
+		input.sendKeys("10051980");
+		input = driver.findElement(By.id("genero"));
+		input.sendKeys("M");
+		input = driver.findElement(By.id("documento"));
+		input.sendKeys(cpf);
+		//input = driver.findElement(By.id("documento2"));
+		//input.sendKeys("rg");
+		input = driver.findElement(By.id("celular"));
+		input.sendKeys("51999999999");
+		input = driver.findElement(By.id("residencial"));
+		input.sendKeys("5133333333");
+		
+		
+		
+		
+		//<input tabindex="1" type="email" value="" required="" name="email" id="email" title="Digite um E-mail válido!" placeholder="E-mail">
+		
+		
+		
+	//	driver.navigate().to("https://www.girafa.com.br/chat-zoo/index.php/por/?site=girafa");
+	//	WebElement input = driver.findElement(By.name("Username"));
+	//	input.submit();
+		
+	//	WebElement element = driver.findElement(By.xpath("//*[@id=\"tituloatendimento\"]"));
+	//	assertTrue(element.getText().contains("Por favor"));
+		
+		
+		
+	}
+	
+	
+	
+	
+	
 }
